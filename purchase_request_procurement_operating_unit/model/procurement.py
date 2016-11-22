ef69c89be1962ff0ec2e6cfd3facd01e154dc81c
darshan-serpent <darshan.patel.serpentcs@gmail.com>
2016-11-22 14:18:04 +0530

    Migrated purchase_request modules and added test cases

--- purchase_request_procurement_operating_unit/model/procurement.py
+++ purchase_request_procurement_operating_unit/model/procurement.py
@@ -1,37 +1,14 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2015 Eficent (<http://www.eficent.com/>)
-#              <contact@eficent.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015 Eficent Business and IT Consulting Services S.L. -
+# Jordi Ballester Alomar
+# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
 from openerp import models, api, _
 
 
 class Procurement(models.Model):
     _inherit = 'procurement.order'
-
-    @api.model
-    def create_procurement_purchase_order(self, procurement,
-                                          po_vals, line_vals):
-        if procurement.location_id:
-            po_vals.update({'operating_unit_id':
-                            procurement.location_id.operating_unit_id.id})
-        return super(Procurement, self).\
-            create_procurement_purchase_order(procurement, po_vals, line_vals)
 
     @api.model
     def _prepare_purchase_request(self, procurement):
