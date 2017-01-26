370bea97fa44b37d054457ecb88cde6a1337eae7
ahenriquez <ahenriquez@eficent.com>
2017-01-26 14:28:58 +0100

    [UPT]References to v10

--- procurement_operating_unit/__init__.py
+++ procurement_operating_unit/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent - Jordi Ballester Alomar
-# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# © 2015-2017 Eficent - Jordi Ballester Alomar
+# © 2015-2017 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
